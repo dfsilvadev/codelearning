f62f8fbba19b84b85fdcf165bfd1c92289eae95d
Daniel Silva <dani3l.dxp@gmail.com>
2021-06-22 08:20:45 -0300

    chore: configurando a estrutura inicial do projeto

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | codelearning.</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          Welcome to codelearning!
+        </h1>        
+      </main>
+    </>
+  )
+}
