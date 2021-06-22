1d4cebe79c325ea5a4aa105444ce3aba9eb03e66
Daniel Silva <dani3l.dxp@gmail.com>
2021-06-22 16:41:30 -0300

    feat: criando as paginas Séries e Sobre para testar o redirect do menu

---
+++ src/pages/series.js
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+export default function Series() {
+  return (
+    <Layout>
+      <h1>Series page</h1>
+    </Layout>
+  );
+}
---
+++ src/pages/sobre.js
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+export default function Sobre() {
+  return (
+    <Layout>
+      <h1>Sobre page</h1>
+    </Layout>
+  );
+}
