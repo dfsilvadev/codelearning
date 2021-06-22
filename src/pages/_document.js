dd88c7209b3dac2d8d866d39734616e6342f7213
Daniel Silva <dani3l.dxp@gmail.com>
2021-06-22 17:59:29 -0300

    feat: ajustando font do projeto

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@400;500;700&family=Roboto:wght@300;400;500;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@400;500;700?family=Inter:wght@400;600;700;900&display=swap"
             rel="stylesheet"
           />
           <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
