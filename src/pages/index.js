6076793426f1782280c4aa0011e50e6804da61f2
Daniel Silva <dani3l.dxp@gmail.com>
2021-06-25 16:27:41 -0300

    feat: finalizando a estrutura da page Home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
+import { Box, Center, Flex, Image, SimpleGrid, Text } from '@chakra-ui/react';
 import Head from 'next/head';
-import Layout from '../components/Layout';
 
-export default function Home() {
+import { getAllTechnologies } from '../services/dato-cms';
+
+import Layout from '../components/Layout';
+import TechnologiesCard from '../components/TechnologiesCard';
+import Footer from '../components/Footer';
+
+export default function Home({ technologies }) {
   return (
     <>
       <Head>
@@ -9,13 +15,51 @@ export default function Home() {
       </Head>
 
       <Layout>
-        <h1>Index</h1>
+        <Flex direction="column" w="100%" h="calc(100vh - 100px)">
+          <SimpleGrid
+            w="100%"
+            maxW={1480}
+            marginX="auto"
+            padding="6"
+            flex="1"
+            columns={[1, null, 2]}
+            spacing="20px"
+            align="center"
+          >
+            <Center>
+              <Flex direction="column" align="flex-start">
+                <Text
+                  as="h1"
+                  fontSize={{ base: '42px', md: '52px', lg: '72px' }}
+                  lineHeight={{ base: '42px', md: '52px', lg: '72px' }}
+                  textAlign="left"
+                  fontWeight="900"
+                >
+                  Aprenda programação direto ao ponto
+                  <Text color="#00ff5f">100% free</Text>
+                </Text>
+              </Flex>
+            </Center>
+            <Center>
+              <Image src="images/learning.svg" />
+            </Center>
+          </SimpleGrid>
+          <Box w="100%" maxW={1480} marginX="auto" padding="6">
+            <TechnologiesCard technologies={technologies} />
+          </Box>
+        </Flex>
+        <Footer />
       </Layout>
     </>
   );
 }
 
-// export const getStaticProps = async () => ({
-//   props: {},
-//   revalidate: 3600,
-// });
+export const getStaticProps = async () => {
+  const technologies = await getAllTechnologies();
+  return {
+    props: {
+      technologies,
+    },
+    revalidate: 3600,
+  };
+};
