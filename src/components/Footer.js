3aab3bdbcaeb04d7a9083284dc00c5cd3420d480
Daniel Silva <dani3l.dxp@gmail.com>
2021-06-25 16:26:52 -0300

    feat: definindo o estilo do footer

---
+++ src/components/Footer.js
@@ -0,0 +1,70 @@
+import {
+  Center,
+  Flex,
+  HStack,
+  IconButton,
+  Link,
+  SimpleGrid,
+  Text,
+} from '@chakra-ui/react';
+import { RiInstagramLine, RiLinkedinBoxLine } from 'react-icons/ri';
+
+export default function Footer() {
+  return (
+    <Flex
+      direction="column"
+      w="100%"
+      h="350px"
+      align="center"
+      background="#000"
+      color="#fff"
+    >
+      <SimpleGrid
+        w="100%"
+        maxW={1480}
+        marginX="auto"
+        padding="6"
+        flex="1"
+        columns={[1, null, 2]}
+        spacing="20px"
+      >
+        <Center>
+          <Text ass="p" fontSize="small">
+            Todos os direitos reservados
+          </Text>
+        </Center>
+        <Center>
+          <HStack spacing="2">
+            <Text as="p" fontSize="small">
+              SIGAM-ME NAS REDES SOCIAIS
+            </Text>
+            <HStack spacing="4">
+              <Link href="https://www.instagram.com/daniel.dxp/">
+                <IconButton
+                  type="sm"
+                  variant="outline"
+                  colorScheme="whiteAlpha"
+                  aria-label="Instagram"
+                  fontSize="1.5rem"
+                  icon={<RiInstagramLine />}
+                  _hover={{ color: '#00ff5f' }}
+                />
+              </Link>
+              <Link href="https://www.linkedin.com/in/daniel-silva-dxp/">
+                <IconButton
+                  type="sm"
+                  variant="outline"
+                  colorScheme="whiteAlpha"
+                  aria-label="Instagram"
+                  fontSize="1.5rem"
+                  icon={<RiLinkedinBoxLine />}
+                  _hover={{ color: '#00ff5f' }}
+                />
+              </Link>
+            </HStack>
+          </HStack>
+        </Center>
+      </SimpleGrid>
+    </Flex>
+  );
+}
