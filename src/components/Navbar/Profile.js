24a2c8eacc8ba39336fdbf04a85dc28bb5b5864a
Daniel Silva <dani3l.dxp@gmail.com>
2021-06-26 19:05:57 -0300

    feat: passando os dados do user logado para o component profile

--- src/components/Navbar/Profile.js
+++ src/components/Navbar/Profile.js
@@ -1,21 +1,24 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
+import useAuth from '../../hooks/useAuth';
 import SignOutMenu from '../SignOutMenu';
 
 export default function Profile({ showProfileData, ...props }) {
+  const { user } = useAuth();
   return (
     <>
       {showProfileData && (
         <Flex align="center">
           <Box marginRight="4" textAlign="right">
-            <Text fontWeight="bold">Daniel Silva</Text>
+            <Text fontWeight="bold">{user.name}</Text>
             <Text color="gray.500" fontSize="small" fontWeight="bold">
-              dfsilva.dxp@gmail.com
+              {user.email}
             </Text>
           </Box>
           <SignOutMenu>
             <Avatar
               size="md"
-              name="Daniel Silva"
+              name={user.name}
+              src={user.photoUrl}
               background="#00ff5f"
               {...props}
             />
