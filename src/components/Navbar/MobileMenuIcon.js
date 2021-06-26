f4fe955ca138a277f3d6b082d181779c7d4735a9
Daniel Silva <dani3l.dxp@gmail.com>
2021-06-26 19:05:29 -0300

    feat: implementando o menu mobile

--- src/components/Navbar/MobileMenuIcon.js
+++ src/components/Navbar/MobileMenuIcon.js
@@ -1,12 +1,30 @@
-import { Box, Flex, Stack } from '@chakra-ui/react';
+import { Box, Button, Flex, Stack, useDisclosure } from '@chakra-ui/react';
+import { useRef } from 'react';
+import ScreenMenu from '../ScreenMenu';
 
 export default function MobileMenuIcon() {
+  const btnMobileRef = useRef();
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <Flex w="10" h="10" align="center">
-      <Stack spacing="2" w="100%">
-        <Box w="100%" h="3px" background="#000" borderRadius="3" />
-        <Box w="100%" h="3px" background="#000" borderRadius="3" />
-      </Stack>
-    </Flex>
+    <>
+      <Button
+        colorScheme="whatsapp"
+        variant="ghost"
+        ref={btnMobileRef}
+        onClick={onOpen}
+      >
+        <Flex w="10" h="10" align="center">
+          <Stack spacing="2" w="100%">
+            <Box w="100%" h="3px" background="#000" borderRadius="3" />
+            <Box w="100%" h="3px" background="#000" borderRadius="3" />
+          </Stack>
+        </Flex>
+      </Button>
+      <ScreenMenu
+        btnMobileRef={btnMobileRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      />
+    </>
   );
 }
