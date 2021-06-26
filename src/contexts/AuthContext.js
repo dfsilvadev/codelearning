bef778b3783ecb16b30a5a22d8cfe9d9e587c0a7
Daniel Silva <dani3l.dxp@gmail.com>
2021-06-26 19:07:55 -0300

    feat: definindo os dados do user e criando a função de login com google

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,24 +1,57 @@
-import Router from "next/router";
-import { createContext, useState } from "react";
+import Router from 'next/router';
+import { createContext, useState } from 'react';
 
-import firebase from "../services/firebase";
+import firebase from '../services/firebase';
 
 const AuthContext = createContext();
+
+const formatUser = async (user) => ({
+  uid: user.uid,
+  email: user.email,
+  name: user.displayName,
+  token: user.za,
+  provider: user.providerData[0].providerId,
+  photoUrl: user.photoURL,
+});
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  async function signIn() {
+  async function handleUser(currentUser) {
+    if (currentUser) {
+      const formattedUser = await formatUser(currentUser);
+      setUser(formattedUser);
+      console.log(currentUser);
+      console.log(formattedUser);
+
+      return formattedUser.email;
+    }
+
+    setUser(false);
+    return false;
+  }
+
+  async function signInWithGithub() {
     try {
       setLoading(true);
-      return await firebase
+      const response = await firebase
         .auth()
-        .signInWithPopup(new firebase.auth.GithubAuthProvider())
-        .then((response) => {
-          setUser(response.user);
-          Router.push("/dashboard");
-        });
+        .signInWithPopup(new firebase.auth.GithubAuthProvider());
+
+      handleUser(response.user);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function signInWithGoogle() {
+    try {
+      const response = await firebase
+        .auth()
+        .signInWithPopup(new firebase.auth.GoogleAuthProvider());
+
+      handleUser(response.user);
     } finally {
       setLoading(false);
     }
@@ -27,7 +60,7 @@ export function AuthProvider({ children }) {
   async function signOut() {
     try {
       setLoading(true);
-      Router.push("/");
+      Router.push('/');
 
       return await firebase
         .auth()
@@ -39,7 +72,9 @@ export function AuthProvider({ children }) {
   }
 
   return (
-    <AuthContext.Provider value={{ user, loading, signIn, signOut }}>
+    <AuthContext.Provider
+      value={{ user, loading, signInWithGithub, signInWithGoogle, signOut }}
+    >
       {children}
     </AuthContext.Provider>
   );
